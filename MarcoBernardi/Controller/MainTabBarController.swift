f67536dade3395d1be5a87c1a5d5df2f3fc20101
MarcoBernardi <marcobernardi.mb97@gmail.com>
2019-05-20 16:43:23 +0200

    Inserita TabBar e relative icone

---
+++ MarcoBernardi/Controller/MainTabBarController.swift
@@ -0,0 +1,47 @@
+//
+//  MainTabBarController.swift
+//  MarcoBernardi
+//
+//  Created by IED Student on 20/05/2019.
+//  Copyright © 2019 IED Student. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let tabBarItem = tabBar.items {
+            
+            let dimensioniIcone = CGSize(width: 30.0, height: 30.0)
+            
+            //Home
+            if tabBarItem.count >= 1 {
+                let itemHome = tabBarItem [0]
+                
+                itemHome.title = "Home"
+                
+                let image = UIImage(named: "iconHome")
+                itemHome.image = UIUtility.resizeImage(image, targetSize: dimensioniIcone)
+            }
+            
+            //Mappa
+            if tabBarItem.count >= 2 {
+                let itemMap = tabBarItem [1]
+                
+                itemMap.title = "Map"
+                
+                let image = UIImage(named: "iconMap")
+                itemMap.image = UIUtility.resizeImage(image, targetSize: dimensioniIcone)
+            }
+        }
+    }
+    
+
+    
+
+    
+
+}
